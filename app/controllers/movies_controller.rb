035b7d793666b47d3d9bffaaaf3ba7f7cc142944
github username <alinakornilova78@gmail.com>
2020-07-10 10:32:36 -0400

    set up both controllers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,40 @@
 class MoviesController < ApplicationController
+
+before_action :set_account
+
+    def index
+       @movies = @account.movies
+       render json: @movies
+    end 
+
+    def create
+        @movie = @account.movies.new(movie_params)
+        if @movie.save 
+            render json: @movie 
+        else
+            render json: {error: 'Error creating account'}
+        end
+    end 
+
+    def show
+      @movies = Movie.find(params[:id)
+      # @movies = @account.find_by(id: params[:id)
+      render json: @movie
+    end 
+
+    def destroy
+    
+    end 
+
+    def set_account
+        @account = Account.find(params[:account_id])
+    end 
+
+    private 
+    
+    def movie_params
+        params.require(:movie).permit(:name, :kind, :account_id)
+    end 
 end
+
+
